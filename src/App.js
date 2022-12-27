dd462870c10f6751f00e633f80add5de9b694026
mozahidul01 <mozo.postbox@gmail.com>
2022-12-27 23:23:46 +0600

    players component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Players from "./components/Players";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-teal-900 text-4xl font-bold text-center">
-        Hello world!
-      </h1>
       <Header />
+      <Players />
     </div>
   );
 }
---
+++ src/components/Players.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Players() {
+  return (
+    <div>
+      <p className="text-2xl font-semibold">Player will show here</p>
+    </div>
+  );
+}
