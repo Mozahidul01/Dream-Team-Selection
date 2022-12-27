9bd5a8c807754e0070f8c05b9ba73a9c7384689d
mozahidul01 <mozo.postbox@gmail.com>
2022-12-27 21:08:02 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-teal-900 text-4xl font-bold text-center">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
